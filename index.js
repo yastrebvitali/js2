859a816d1d3b39224f39014a75916876de5dff85
yastrebvitali <yastrebvitali@gmail.com>
2021-02-08 14:46:14 +0300

    js2

---
+++ index.js
@@ -0,0 +1,13 @@
+var massiv= {
+    1:1,
+     2:true,
+    true:2,
+    'name':'KOlan',
+    3:'free'
+
+}
+console.log(massiv);
+console.log(massiv["1"]);
+console.log(massiv['2']);
+console.log(massiv['name']);
+
